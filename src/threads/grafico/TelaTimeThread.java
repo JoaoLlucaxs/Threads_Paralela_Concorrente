a5b1b8c740ce03ca3a6a7fae3d82c2774e0fd6ee
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-05-24 14:11:46 -0300

    ..

--- src/threads/grafico/TelaTimeThread.java
+++ src/threads/grafico/TelaTimeThread.java
@@ -8,6 +8,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -27,6 +29,9 @@ public class TelaTimeThread extends JDialog{
     private JLabel descricaoHora2=new  JLabel("Time Thread 2");
     private JTextField mostraTempo2=new JTextField();
     
+    private JButton botaoStart=new JButton("Start");
+    private JButton botaoStop=new JButton("Stop");
+    
     public TelaTimeThread(){
         setTitle("Minha tela para Threads");
         setSize(new Dimension(240,240));
@@ -36,12 +41,16 @@ public class TelaTimeThread extends JDialog{
         GridBagConstraints bagConstraints=new GridBagConstraints(); // controlador de posicionamento de componentes
         bagConstraints.gridx=0;
         bagConstraints.gridy=0;
+        bagConstraints.gridwidth=2;
+        bagConstraints.insets=new Insets(5, 10, 5, 5);
+        bagConstraints.anchor=GridBagConstraints.WEST;
         
         descricaoHora.setPreferredSize(new Dimension(200,25));
         jPanel.add(descricaoHora, bagConstraints);
         
         mostraTempo.setPreferredSize(new Dimension(200,25));
         bagConstraints.gridy ++;
+        mostraTempo.setEditable(false);
         jPanel.add(mostraTempo,bagConstraints);
         
         
@@ -51,8 +60,19 @@ public class TelaTimeThread extends JDialog{
         
         mostraTempo2.setPreferredSize(new Dimension(200,25));
        bagConstraints.gridy ++;
+       mostraTempo2.setEditable(false);
        jPanel.add(mostraTempo2,bagConstraints);
-        
+       
+       bagConstraints.gridwidth=1;
+       
+       botaoStart.setPreferredSize(new Dimension(92,25));
+       bagConstraints.gridy++;
+       jPanel.add(botaoStart,bagConstraints);
+       
+       botaoStop.setPreferredSize(new Dimension(92,25));
+       bagConstraints.gridx++;
+       jPanel.add(botaoStop,bagConstraints);
+               
         add(jPanel,BorderLayout.WEST);
         // sempre será o último comando
         setVisible(true); // torna a tela visivel para usuário
